4849335df6ef8278a4d2205ac41e412d4b95c975
ZerpZhi <zekazilin@gmail.com>
2015-05-14 23:19:30 +0300

    sozdan HW3, 2) & 3) zadanie

---
+++ geometria.php
@@ -0,0 +1,27 @@
+<?php
+
+function treugolnik($a,$h)
+ {
+   $s=0.5*$a*$h;
+   echo 'S треугольника ='. $s."<br>";
+ }
+
+  treugolnik(5, 4);
+
+function priamougolnik($a,$b)
+ {
+   $s=$a*$b;
+   echo 'S прямоугольника ='. $s."<br>";
+ }
+
+  priamougolnik(3, 4);
+
+function krug($r)
+ {
+   $s=3.141592*($r*$r);
+   echo 'S круга ='. $s."<br>";
+ }
+
+  krug(452);
+
+?>
